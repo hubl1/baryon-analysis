7e6cbc0ac2a68ea94826b8416ac06de2c04f74d4
hubl1 <hubolun1@gmail.com>
2025-05-23 17:28:40 +0800

    Update env.sh

--- codes/params/env.sh
+++ codes/params/env.sh
@@ -3,3 +3,6 @@
 lqcd_data_path='/lqcd_data_path'
 parent="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"/.. && pwd )"
 export PYTHONPATH="$parent:$PYTHONPATH"
+
+mkdir -p "$lqcd_data_path/figures"
+mkdir -p "$lqcd_data_path/final_results"
