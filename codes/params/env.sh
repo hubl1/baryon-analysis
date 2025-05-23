b5255ab645113baf84bf7b259ba28a6f453face8
hubl1 <hubolun1@gmail.com>
2025-05-23 16:04:12 +0800

    Update env.sh

--- codes/params/env.sh
+++ codes/params/env.sh
@@ -1,6 +1,5 @@
 #!/bin/bash 
 
-# lqcd_data_path='/lqcd_data_path'
-lqcd_data_path='/Users/hubl/lqcd_data'
+lqcd_data_path='/lqcd_data_path'
 parent="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"/.. && pwd )"
 export PYTHONPATH="$parent:$PYTHONPATH"
